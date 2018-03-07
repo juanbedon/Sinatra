6ed6d12cecaf6c8f8c775e190384ab84b90a53ed
Juan <juanbedon26@hotmail.com>
2018-03-07 14:21:18 -0500

    Cambio en .rb

--- solution.rb
+++ solution.rb
@@ -1,6 +1,20 @@
 require 'sinatra'
 
-get '/:nombre' do
-  @nombre = "#{params['nombre'] ? params['nombre'].capitalize : 'desconocido!'}!"
-erb :index
+get '/' do
+	unless params[:nombre]
+		"Hola mundo"
+	else
+		"Hola #{params[:nombre]}"
+	end
+	erb :index
 end
+
+get '/:nombre' do
+	"Hola #{params[:nombre]}"
+end
+
+=begin
+post '/nuevo/objeto' do
+	"¡Hola #{params[:nombre]}!"
+end
+=end
